9a63902954d55f6e6de2ef9b8987652c14e59d54
mbaranr <mbaranr03@gmail.com>
2025-09-22 12:49:21 +0200

    initial changes

--- _plugins/external-posts.rb
+++ _plugins/external-posts.rb
@@ -71,6 +71,8 @@ module ExternalPosts
         puts "...fetching #{post['url']}"
         content = fetch_content_from_url(post['url'])
         content[:published] = parse_published_date(post['published_date'])
+        content[:title] = post['title'] if post['title']
+        content[:summary] = post['summary'] if post['summary']
         create_document(site, src['name'], post['url'], content)
       end
     end
